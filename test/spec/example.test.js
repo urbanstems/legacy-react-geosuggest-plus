6c768bb7f9930fb081bb9aefcf1b555c84c4459c
Dustin <dustin@urbanstems.com>
2017-02-21 09:44:51 -0500

    Update testing setup

---
+++ test/spec/example.test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+describe('Somethang...', function () {
+  it('should pass', function () {
+    expect(true).to.be.true;
+  });
+});
