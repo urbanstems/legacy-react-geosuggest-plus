7ca94cdca86c276205ae60b50609ccda2a99d055
Dustin <dustin@urbanstems.com>
2017-02-21 10:02:30 -0500

    <Add RecentListItem />

---
+++ src/components/RecentsListItem.js
@@ -0,0 +1,27 @@
+import React from 'react'; // eslint-disable-line
+
+const RecentListItem = ({ classes, onClick, suggest }) => (
+  <li className={classes} onClick={onClick}>
+    <span className="icon icon-house" />
+    <strong>
+      {suggest.firstname}{'\u0020'}
+      {suggest.lastname}
+    </strong><br />
+    {suggest.place_name}
+    {suggest.address1}{suggest.address2 ? ` ${suggest.address2}` : ''},{'\u0020'}
+    {suggest.city}, {suggest.state_name}
+  </li>
+);
+
+RecentListItem.propTypes = {
+  classes: React.PropTypes.string,
+  suggest: React.PropTypes.object.isRequired,
+  onClick: React.PropTypes.func.isRequired,
+};
+
+RecentListItem.defaultProps = {
+  classes: '',
+  onClick() {},
+};
+
+export default RecentListItem;
